ba513903890087a3ed47d6d853f5f58ffda95e51
Renato Costa <renatocostatristao@gmail.com>
2023-09-10 15:22:00 -0300

    criando classe de encrypt de senha

--- auth/models/user.ts
+++ auth/models/user.ts
@@ -1,4 +1,5 @@
 import {Schema, model } from "mongoose";
+import { Password } from "../src/services/password";
 
 interface IUser {
   email: string;
@@ -14,6 +15,14 @@ const userSchema = new Schema<IUser>({
     type: String,
     required: true,
    }
+})
+
+userSchema.pre('save', async function(done) {
+  if(this.isModified('password')){
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 })
 
 const User = model<IUser>('User', userSchema);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, {type Request, type Response} from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -24,8 +25,7 @@ router.post('/api/users/signup', [
   const existingUser = await User.findOne({ email });
 
   if(existingUser){
-    console.log('Email in use');
-    return res.send({});
+    throw new BadRequestError('Email already in use')
   }
 
   const user = new User({ email, password });
