833301382ab7f36aeafe5bd4e54455564f9405bd
Renato Costa <renatocostatristao@gmail.com>
2023-09-10 15:25:04 -0300

    configurando cookiesession ao servidor

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors"
+import cookieSession from "cookie-session";
 import { json } from "body-parser"
 import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
@@ -10,7 +11,14 @@ import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
 
 const app = express()
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+)
 
 
 
