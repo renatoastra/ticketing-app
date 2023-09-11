ff9a8900b573a6caaa0383e417627985c6d7396d
Renato Costa <renatocostatristao@gmail.com>
2023-09-10 23:12:34 -0300

    configure current user router

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,10 @@
 import express, { type Request, type Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from "jsonwebtoken"
 
 const router = express.Router();
 
@@ -17,6 +20,30 @@ router.post('/api/users/signin',
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+     }
+
+    const passwordMatch = await Password.compare(existingUser.password, password);
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+     }
+     //generate jsonwebtoken
+    const userJwt = jwt.sign({
+      id: existingUser.id,
+      email: existingUser.email
+    }, process.env.JWT_KEY!)
+
+    req.session = {
+      jwt: userJwt
+    }
+
+  res.status(200).send(existingUser);
 })
 
 export { router as signInRouter };
